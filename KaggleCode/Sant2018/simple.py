1cbe783b9329e765fae08936f0c99fd789c6f1c0
wzswan <wzswan@gmail.com>
2018-07-01 19:04:50 +0900

    baseline without KFold

--- KaggleCode/Sant2018/simple.py
+++ KaggleCode/Sant2018/simple.py
@@ -6,6 +6,7 @@ import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
 
 import os
 import gc
+import copy
 #print(os.listdir("../input"))
 
 # Models Packages
@@ -22,72 +23,103 @@ import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
 from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
 from sklearn.decomposition import TruncatedSVD
 from sklearn import preprocessing, model_selection, metrics
-from sklearn.pipline import FeatureUnion
+from sklearn.pipeline import FeatureUnion
 from scipy.sparse import hstack, csr_matrix
+from sklearn.preprocessing import scale
+from sklearn.decomposition import PCA
 #
 # Fradient Boosting
 import lightgbm as lgb
 from sklearn.linear_model import Ridge
 from sklearn.cross_validation import KFold
 
-NFOLDS = 5
-SEED = 42
-VALID = True
 
-class SklearnWrapper(object):
-    def __init__(self, clf, seed=0, params=None, seed_bool = True):
-        if(seed_bool == True):
-            params['random_state'] = seed
-        self.clf = clf(**params)
-
-    def train(self, x_train, y_train):
-        self.clf.fit(x_train, y_train)
-
-    def predict(self, x):
-        return self.clf.predict(x)
-
-def get_oof(clf, x_train, y, x_test):
-    oof_train = np.zeros((ntrain,))
-
-
-
+print("\nData Load Stage")
 train_df = pd.read_csv("../input/train.csv")
+traindex = train_df.index
 test_df = pd.read_csv("../input/test.csv")
+testdex = test_df.index
+test_id = test_df['ID'].values
+y = train_df['target'].values
 
-total_df = pd.concat([train_df.drop('target', axis=1), test_df], axis=0).drop('ID', axis=1)
+print('Train shape: {} Rows, {} Columns'.format(*train_df.shape))
+print('Test shape: {} Rows, {} Columns'.format(*test_df.shape))
 
-# Columns to drop because there is no variation in training set
-zero_std_cols = train_df.drop("ID", axis=1).columns[train_df.std() == 0]
-total_df.drop(zero_std_cols, axis=1, inplace=True)
-print(f">> Removed {len(zero_std_cols)} constant columns")
-
-# Removing duplicate columns
-# Taken from: https://www.kaggle.com/scirpus/santander-poor-mans-tsne
 colsToRemove = []
-colsScaned = []
-dupList = {}
-columns = total_df.columns
-for i in range(len(columns)-1):
-    v = train_df[columns[i]].values
-    dupCols = []
-    for j in range(i+1,len(columns)):
-        if np.array_equal(v, train_df[columns[j]].values):
-            colsToRemove.append(columns[j])
-            if columns[j] not in colsScaned:
-                dupCols.append(columns[j])
-                colsScaned.append(columns[j])
-                dupList[columns[i]] = dupCols
-colsToRemove = list(set(colsToRemove))
-total_df.drop(colsToRemove, axis=1, inplace=True)
-print(f">> Dropped {len(colsToRemove)} duplicate columns")
+for col in train_df.columns:
+    if col != 'ID' and col != 'target':
+        if train_df[col].std() == 0:
+            colsToRemove.append(col)
 
-# Go through the columns one at a time (can't do it all at once for this dataset)
-total_df_all = deepcopy(total_df)
+# remove constant columns in the training set
+train_df.drop(colsToRemove, axis=1, inplace=True)
+
+# remove constant columns in the test set
+test_df.drop(colsToRemove, axis=1, inplace=True)
+
+print("Removed `{}` Constant Columns\n".format(len(colsToRemove)))
+
+def duplicate_columns(frame):
+    groups = frame.columns.to_series().groupby(frame.dtypes).groups
+    dups = []
+
+    for t, v in groups.items():
+
+        cs = frame[v].columns
+        vs = frame[v]
+        lcs = len(cs)
+
+        for i in range(lcs):
+            ia = vs.iloc[:,i].values
+            for j in range(i+1, lcs):
+                ja = vs.iloc[:,j].values
+                if np.array_equal(ia, ja):
+                    dups.append(cs[i])
+                    break
+
+    return dups
+
+colsToRemove = duplicate_columns(train_df)
+#print(colsToRemove)
+
+train_df.drop(colsToRemove, axis=1, inplace=True)
+
+# remove duplicate columns in the testing set
+test_df.drop(colsToRemove, axis=1, inplace=True)
+
+print("Removed `{}` Duplicate Columns\n".format(len(colsToRemove)))
+
+def drop_sparse(train, test):
+    flist = [x for x in train.columns if not x in ['ID','target']]
+    for f in flist:
+        if len(np.unique(train[f]))<2:
+            train.drop(f, axis=1, inplace=True)
+            test.drop(f, axis=1, inplace=True)
+    return train, test
+
+%%time
+train_df, test_df = drop_sparse(train_df, test_df)
+
+gc.collect()
+print("Train set size: {}".format(train_df.shape))
+print("Test set size: {}".format(test_df.shape))
+
+print('save into csv file')
+train_df.to_csv("prepro_train_df.csv", index=False)
+test_df.to_csv("prepro_total_df_all.csv", index=False)
+print("Combine Train and Test")
+total_df = pd.concat([train_df.drop('target', axis=1), test_df], axis=0).drop('ID', axis= 1)
+#del train_df, test_df
+gc.collect()
+
+total_df_all = copy.deepcopy(total_df)
 for col in total_df.columns:
 
     # Detect outliers in this column
     data = total_df[col].values
     data_mean, data_std = np.mean(data), np.std(data)
+
+
     cut_off = data_std * 3
     lower, upper = data_mean - cut_off, data_mean + cut_off
     outliers = [x for x in data if x < lower or x > upper]
@@ -108,17 +140,26 @@ for col in total_df.columns:
 # Train and test
 train_idx = range(0, len(train_df))
 test_idx = range(len(train_df), len(total_df))
+
+total_df_all['__data_sum'] = 0
+for col in total_df_all.columns:
+
+    data = total_df_all[col].values
+    #total_df_all[col + '_data_mean'] = data_mean
+    #total_df_all[col + '_data_std'] = data_std
+    total_df_all['__data_sum'] += data
+
+total_df_all['__data_mean'] = total_df_all['__data_sum'] / len(total_df_all)
+
 print('save into csv file')
 total_df.to_csv("total_df.csv", index=False)
 total_df_all.to_csv("total_df_all.csv", index=False)
-
-print('total_df_all rows and col', total_df_all.shape)
 
 def test_pca(data, create_plots=True):
     """Run PCA analysis, return embedding"""
 
     # Create a PCA object, specifying how many components we wish to keep
-    pca = PCA(n_components=1000)
+    pca = PCA(n_components=100)
 
     # Run PCA on scaled numeric dataframe, and retrieve the projected data
     pca_trafo = pca.fit_transform(data)
@@ -159,32 +200,73 @@ def test_pca(data, create_plots=True):
     return pca_df
 
 # Run the PCA and get the embedded dimension
-pca_df = test_pca(total_df)
+#pca_df = test_pca(total_df)
 pca_df_all = test_pca(total_df_all, create_plots=False)
-print('save pca file')
-pca_df.to_csv("pca_df.csv", index=False)
+
+print('save into csv file')
+#total_df.to_csv("total_df.csv", index=False)
 pca_df_all.to_csv("pca_df_all.csv", index=False)
 
-def test_prediction(data):
-    """Try to classify train/test samples from total dataframe"""
+X = df.loc[traindex,:].values
+test_df = df.loc[testdex,:].values
 
-    # Create a target which is 1 for training rows, 0 for test rows
-    y = np.zeros(len(data))
-    y[train_idx] = 1
+for shape in [X,test_df]:
+    print("{} Rows and {} Cols".format(*shape.shape))
 
-    # Perform shuffled CV predictions of train/test label
-    predictions = cross_val_predict(
-        ExtraTreesClassifier(n_estimators=100, n_jobs=4),
-        data, y,
-        cv=StratifiedKFold(
-            n_splits=10,
-            shuffle=True,
-            random_state=42
-        )
+print("Light Gradient Boosting Regressor")
+lgbm_params =  {
+    'task': 'train',
+    'boosting_type': 'gbdt',
+    'objective': 'regression',
+    'metric': 'rmse',
+    # 'max_depth': 15,
+    'num_leaves': 270,
+    'feature_fraction': 0.5,
+    'bagging_fraction': 0.75,
+    'bagging_freq': 2,
+    'learning_rate': 0.016,
+    'verbose': 0
+}
+
+if VALID == False:
+    X_train, X_valid, y_train, y_valid = train_test_split(
+        X, y, test_size=0.10, random_state=23)
+
+    # LGBM Dataset Formatting
+    lgtrain = lgb.Dataset(X_train, y_train)
+    lgvalid = lgb.Dataset(X_valid, y_valid)
+    del X, X_train; gc.collect()
+
+    # Go Go Go
+    lgb_clf = lgb.train(
+        lgbm_params,
+        lgtrain,
+        num_boost_round=20000,
+        valid_sets=[lgtrain, lgvalid],
+        valid_names=['train','valid'],
+        early_stopping_rounds=50,
+        verbose_eval=100
+    )
+    print("Model Evaluation Stage")
+    print('RMSE:', np.sqrt(metrics.mean_squared_error(y_valid, lgb_clf.predict(X_valid))))
+    del X_valid ; gc.collect()
+
+else:
+    # LGBM Dataset Formatting
+    lgtrain = lgb.Dataset(X, y)
+    del X; gc.collect()
+    # Go Go Go
+    lgb_clf = lgb.train(
+        lgbm_params,
+        lgtrain,
+        num_boost_round=1550,
+        verbose_eval=100
     )
 
-    # Show the classification report
-    print(classification_report(y, predictions))
+lgpred = lgb_clf.predict(test_df)
 
-# Run classification on total raw data
-test_prediction(total_df_all)
+#Mixing lightgbm with ridge. I haven't really tested if this improves the score or not
+#blend = 0.95*lgpred + 0.05*ridge_oof_test[:,0]
+lgsub = pd.DataFrame(lgpred,columns=["target"],index=testdex)
+lgsub['target'].clip(0.0, 1.0, inplace=True) # Between 0 and 1
+lgsub.to_csv("lgsub.csv",index=True,header=True)
